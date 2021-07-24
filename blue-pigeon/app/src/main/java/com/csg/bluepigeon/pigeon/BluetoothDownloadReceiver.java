63597e3444321b5c2f032a24d132af2a88bcad39
chia hui <mahchiahui@hotmail.sg>
2021-07-24 22:58:45 +0800

    #1 updated code to accommodate new payload format, added more code to include metadata, added comments

--- blue-pigeon/app/src/main/java/com/csg/bluepigeon/pigeon/BluetoothDownloadReceiver.java
+++ blue-pigeon/app/src/main/java/com/csg/bluepigeon/pigeon/BluetoothDownloadReceiver.java
@@ -16,26 +16,27 @@ import com.csg.bluepigeon.util.BlueChirp;
 import com.csg.bluepigeon.util.Config;
 
 public class BluetoothDownloadReceiver extends BroadcastReceiver {
-    final String TAG = "BTDownloadReceiver";
+    final String TAG = "BluetoothDownloadReceiver ";
     private BlueChirp blueChirp = new BlueChirp(Config.LOGGING_DIR_PATH);
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        // Extract Blue Pigeon Address
+
+        // Extract metadata from bluedispatcher and bluepigeon to be sent to c2
         BluetoothDevice bluetoothDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-        String bpAddress = bluetoothDevice.getAddress();
-        String bpName = bluetoothDevice.getName();
-        String blueCoopName = BluetoothAdapter.getDefaultAdapter().getName();
+        String blueDispAddress = bluetoothDevice.getAddress();
+        String blueDispName = bluetoothDevice.getName();
+        String bluePigName = BluetoothAdapter.getDefaultAdapter().getName();
 
         // Put into Data
         Data bpData = new Data.Builder()
-                .putString("address", bpAddress)
-                .putString("bpName", bpName)
-                .putString("blueCoopName", blueCoopName)
+                .putString("blueDispAddress", blueDispAddress)
+                .putString("blueDispName", blueDispName)
+                .putString("bluePigName", bluePigName)
                 .build();
 
         // start blue pigeon listener
-        blueChirp.log("onReceive: starting blue pigeon on address " + bpAddress, "info");
+        blueChirp.log(TAG + "onReceive: deploying blue pigeon to deliver message from device name: " + blueDispName + ", mac address: " + blueDispAddress, "info");
 
         WorkManager workManager = WorkManager.getInstance(context);
         workManager.enqueue(new OneTimeWorkRequest.Builder(BluePigeonListener.class).setInputData(bpData).build());
